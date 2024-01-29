083fb3abf31b0d17c979b61b70133fbe6ce6efbf
Lobiofrom <124072945+Lobiofrom@users.noreply.github.com>
2024-01-29 19:18:28 +0300

    n

--- data/src/main/java/com/example/data/remote/KtorServiceImpl.kt
+++ data/src/main/java/com/example/data/remote/KtorServiceImpl.kt
@@ -6,19 +6,15 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.url
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class KtorServiceImpl(
     private val client: HttpClient
 ) : KtorService {
     override suspend fun getPosts(): Response {
-        return withContext(Dispatchers.IO) {
-            client.get {
-                url(HttpRoutes.BASE_URL)
-                parameter("locale", "en-US")
-                parameter("limit", 10)
-            }
+        return client.get {
+            url(HttpRoutes.BASE_URL)
+            parameter("locale", "en-US")
+            parameter("limit", 10)
         }
     }
 }
